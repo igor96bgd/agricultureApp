0e1f9155493771c9f2a2ec5e39f3cd463565b1c9
igor96bgd <93517519+igor96bgd@users.noreply.github.com>
2021-11-01 14:47:27 +0100

    Delete client/src/models directory

--- client/src/models/magacin.ts
+++
@@ -1,8 +0,0 @@
-export class Magacin{
-    naziv: string;
-    proizvodjac: string;
-    poljoprivrednik: string;
-    kolicina: number;
-    dani: number;
-    tip: string;
-}
--- client/src/models/sadnica.ts
+++
@@ -1,8 +0,0 @@
-export class Sadnica{
-    naziv: string;
-    proizvodjac: string;
-    dani: number;
-    zavrseno: number;
-    vadjenje: boolean;
-    vreme: string;
-}
